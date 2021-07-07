80bc26fdda7badd86705fa963aff01f6528f868a
Nico Beierle <nico.beierle@gmail.com>
2021-07-07 22:24:17 +0200

    action to automate tests

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,12 @@ module.exports = {
     '^.+\\.js$': 'babel-jest',
   },
   moduleFileExtensions: ['js', 'svelte'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
